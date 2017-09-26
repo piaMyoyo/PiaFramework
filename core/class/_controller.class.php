ffb7f4c8a4d1aec6cf6f81e0a2b7325f3db864df
PiaMyoyo <anthony.millot@outlook.com>
2017-09-27 00:23:53 +0200

    prepare controller / layout layer

--- core/class/_controller.class.php
+++ core/class/_controller.class.php
@@ -5,11 +5,15 @@ namespace pia\core;
 abstract class _Pia_Controller
 {
 
+    private $_OUTPUT;
     private $_MODELS;
-
 
     public function __construct(){
 
+    }
+
+    protected function prepare(){
+        
     }
 
     protected function render(){
@@ -20,8 +24,16 @@ abstract class _Pia_Controller
 
     }
 
-    protected function getModel($path){
+    protected function addLayoutItem($type, $path){
+        
+    }
 
+    protected function loadModel($model){
+        // Charge un model
+    }
+
+    protected function getModel($path){
+        // Appel un model chargé
     }
 
 }
